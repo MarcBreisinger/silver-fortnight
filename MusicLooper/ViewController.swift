0e4dd2aff7ad9f0bc36257560bcf2643a2caeded
Marc Breisinger <marcbreisinger@marcs-mbp.fritz.box>
2017-11-03 10:32:31 +0100

    Initial Commit

---
+++ MusicLooper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicLooper
+//
+//  Created by Marc Breisinger on 03.11.17.
+//  Copyright © 2017 Marc Breisinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
